954bc19cabe681e9a0498c1825a7238474a84928
HafizZada16 <hafizzada16@gmail.com>
2025-11-08 18:39:15 +0700

    Menambah ListOf FormData

--- app/src/main/java/com/example/questnavigasitugas_016/viewmodel/ParticipantViewModel.kt
+++ app/src/main/java/com/example/questnavigasitugas_016/viewmodel/ParticipantViewModel.kt
@@ -1,7 +1,36 @@
 package com.example.questnavigasitugas_016.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.questnavigasitugas_016.data.FormData
 
 class ParticipantViewModel : ViewModel(){
-
+    private val _participants = MutableLiveData<List<FormData>>(
+        listOf(
+            FormData(
+                nama = "Asroni Sukirman",
+                gender = "Laki - Laki",
+                status = "Cerai",
+                alamat = "Sleman"
+            ),
+            FormData(
+                nama = "Aprilia Kurnianti",
+                gender = "Perempuan",
+                status = "Lajang",
+                alamat = "Bantul"
+            ),
+            FormData(
+                nama = "Haris Setyawan",
+                gender = "Laki - Laki",
+                status = "Kawin",
+                alamat = "Jogja"
+            ),
+            FormData(
+                nama = "Hafiz",
+                gender = "Laki - Laki",
+                status = "Lajang",
+                alamat = "Pacitan"
+            )
+        )
+    )
 }
