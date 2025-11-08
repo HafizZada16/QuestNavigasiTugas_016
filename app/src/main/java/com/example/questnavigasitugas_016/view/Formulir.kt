dfc884ee8e09844b15d77290e800d5c319b8633a
HafizZada16 <hafizzada16@gmail.com>
2025-11-08 20:14:52 +0700

    Menambah fungsi Button back dan Submit

--- app/src/main/java/com/example/questnavigasitugas_016/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasitugas_016/view/Formulir.kt
@@ -1,5 +1,6 @@
 package com.example.questnavigasitugas_016.view
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,8 +10,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
@@ -24,6 +28,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
@@ -132,6 +137,31 @@ fun FormulirScreen(navController: NavController, viewModel: ParticipantViewModel
                     .height(100.dp),
                 keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done)
             )
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                OutlinedButton(
+                    onClick = { navController.popBackStack() },
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(50.dp)
+                ) {
+                    Text(stringResource(id = R.string.back_button))
+                }
+                Spacer(modifier = Modifier.width(16.dp))
+                Button(
+                    onClick = handleSubmit,
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(50.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF8A2BE2))
+                ) {
+                    Text(stringResource(id = R.string.submit_button), color = Color.White)
+                }
+            }
         }
     }
 }
