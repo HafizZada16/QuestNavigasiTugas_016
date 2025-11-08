239f84892a34daa7bc317a178471ebea17f7d20a
HafizZada16 <hafizzada16@gmail.com>
2025-11-08 18:52:59 +0700

    Menambah Tampilan Selamat Datang

--- app/src/main/java/com/example/questnavigasitugas_016/view/Welcome.kt
+++ app/src/main/java/com/example/questnavigasitugas_016/view/Welcome.kt
@@ -2,13 +2,19 @@ package com.example.questnavigasitugas_016.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.questnavigasitugas_016.R
 
 @Composable
 fun WelcomeScreen(navController: NavController) {
@@ -18,5 +24,11 @@ fun WelcomeScreen(navController: NavController) {
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
-    ) {}
+    ) {
+        Text(
+            text = stringResource(R.string.welcome_title),
+            style = MaterialTheme.typography.headlineMedium
+        )
+        Spacer(modifier = Modifier.weight(1f))
+    }
 }
