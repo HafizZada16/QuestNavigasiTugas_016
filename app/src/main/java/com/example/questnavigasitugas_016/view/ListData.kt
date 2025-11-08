759f6292fa19419849555599bfc437ff7e5bc8c8
HafizZada16 <hafizzada16@gmail.com>
2025-11-08 19:23:50 +0700

    Menambah ListData.kt

---
+++ app/src/main/java/com/example/questnavigasitugas_016/view/ListData.kt
@@ -0,0 +1,10 @@
+package com.example.questnavigasitugas_016.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.example.questnavigasitugas_016.viewmodel.ParticipantViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ListDataScreen(navController: NavController, viewModel: ParticipantViewModel) {}
