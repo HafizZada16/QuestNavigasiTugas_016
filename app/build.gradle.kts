89e32dee7985e38155bcbc58b351548c500a455d
HafizZada16 <hafizzada16@gmail.com>
2025-11-08 19:29:26 +0700

    Menambah Scaffold topBar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.compose.runtime.livedata)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/questnavigasitugas_016/view/ListData.kt
+++ app/src/main/java/com/example/questnavigasitugas_016/view/ListData.kt
@@ -1,10 +1,34 @@
 package com.example.questnavigasitugas_016.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
+import com.example.questnavigasitugas_016.R
 import com.example.questnavigasitugas_016.viewmodel.ParticipantViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ListDataScreen(navController: NavController, viewModel: ParticipantViewModel) {}
+fun ListDataScreen(navController: NavController, viewModel: ParticipantViewModel) {
+    val participants by viewModel.participants.observeAsState(initial = emptyList())
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.participant_list_title)) },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.surface,
+                    titleContentColor = MaterialTheme.colorScheme.onSurface
+                )
+            )
+        },
+
+    ){}
+}
